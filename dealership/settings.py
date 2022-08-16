b56b99c808001ae6995aded8c73e401fe535f31f
AB Sharif <absharif.cs@gmail.com>
2022-08-16 09:54:30 +0600

    setting updated

--- dealership/settings.py
+++ dealership/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/sammy/myprojectdir/static_cdn/static_root/static_root/static'
+STATIC_ROOT = '/home/sammy/myprojectdir/static_cdn/static_root/static'
 #STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn/static_root/static')
 STATICFILES_DIRS = [
     # os.path.join(os.path.dirname(BASE_DIR), 'static'),
