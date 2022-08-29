6ea3525f4a68345c3b78f5d7d6cdedb9d214e830
AB Sharif <absharif.cs@gmail.com>
2022-08-29 19:23:22 +0600

    Deploy issue

--- dealership/settings.py
+++ dealership/settings.py
@@ -80,20 +80,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dealership.wsgi.application'
 
-#
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL')
-#     )
-# }
-
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
